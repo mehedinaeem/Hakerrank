32f5736a49f466165525f103d6e1424dd133b3c6
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-16 10:33:25 +0600

    c++

---
+++ 30_DAY_CHLG/DAY-08.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        getline(cin,s1);
+        cin>>s2;
+
+        cout<<s1<<endl;
+        cout<<s2<<endl;
+
+    //     while(s2.length()=s1.length())
+    //     {
+    //         if(s2[i]=s1[i])
+    //         {
+    //             cout<<s2<<"="<<s1<<endl;
+    //         }
+    //         else
+    //         cout<<"Not found"<<endl;
+    //     }
+    // }
+//     for(i=0;i<n;i++)
+//    {
+//        cout<<s1<<endl;
+//        cout<<s2<<endl;
+//    }
+    return 0;
+}
--- 30_DAY_CHLG/tempCodeRunnerFile.cpp
+++ 30_DAY_CHLG/tempCodeRunnerFile.cpp
@@ -1,5 +1,11 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int main()
-{
+    //     while(s2.length()=s1.length())
+    //     {
+    //         if(s2[i]=s1[i])
+    //         {
+    //             cout<<s2<<"="<<s1<<endl;
+    //         }
+    //         else
+    //         cout<<"Not found"<<endl;
+    //     }
+    // }
